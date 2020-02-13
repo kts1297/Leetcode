fb16c6d29008987d54f48e817f3c3d808db8b8dc
Teja Srinivas <kamisetti.s@tallysolutions.com>
2020-02-13 17:25:42 +0530

    Final code

--- Two Sum.py
+++ Two Sum.py
@@ -1,7 +1,13 @@
 # https://leetcode.com/problems/two-sum/
 
+from collections import defaultdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums.sort()
-        start, end = 0, len(nums)-1
-        res =
+        dic = defaultdict(int)
+        res = []
+        for x in range(len(nums)):
+            if target-nums[x] in dic:
+                res.append(dic[target-nums[x]])
+                res.append(x)
+                return res
+            dic[nums[x]] = x
